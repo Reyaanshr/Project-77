044593c483884d21a0839f60ea8427ee5c5fa988
Reyaanshr <76482585+Reyaanshr@users.noreply.github.com>
2021-05-06 21:24:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+var first_name = "Reyaansh";
+var last_name = "Roy";
+console.log(first_name);
+console.log(last_name);
+var full_name = first_name.concat(last_name);
+console.log(full_name)
+var a = 23;
+var b = 54;
+console.log(a);
+console.log(b);
+var add = a + b;
+console.log(add)
+var subtract = a - b;
+console.log(subtract)
+var multiply = a * b;
+console.log(multiply);
+var divide = a / b;
+console.log(divide);
